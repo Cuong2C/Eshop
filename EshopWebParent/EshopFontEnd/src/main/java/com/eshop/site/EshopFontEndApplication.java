a43d04e31a91c0800a0eff837095c1a799d5a1f7
Cuong2C <dcongcuong2012@gmail.com>
2023-08-08 13:51:18 +0700

    first commit

---
+++ EshopWebParent/EshopFontEnd/src/main/java/com/eshop/site/EshopFontEndApplication.java
@@ -0,0 +1,13 @@
+package com.eshop.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopFontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopFontEndApplication.class, args);
+	}
+
+}
